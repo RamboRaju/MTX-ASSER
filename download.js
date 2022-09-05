8a3a6d76a3ccbc886948bafd5144de743bbc1a58
RamboRaju <42264377+RamboRaju@users.noreply.github.com>
2022-09-05 12:18:32 +0530

    Create download.js

---
+++ download.js
@@ -0,0 +1,5 @@
+// Download file in community 
+// first goto -> setup -> search -> file upload and download security -> change you file extention to download
+// similary if you want yo preview you make open in the browser
+
+"/sfc/servlet.shepherd/document/download/" +this.fileList[i].ContentDocumentId
